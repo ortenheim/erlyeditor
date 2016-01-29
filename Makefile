35b3524a98f1fd023bb591e91ea4103c8d913e1c
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2016-01-29 20:03:22 +0300

    chore(init): Add basic config files for babel, CI, linters, etc.

---
+++ Makefile
@@ -0,0 +1,8 @@
+start:
+	npm start
+test:
+	npm test
+build:
+	npm run build
+
+.PHONY: start test build
---
+++ styleling.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "extends": "stylelint-config-suitcss",
+  "rules": {
+    "at-rule-empty-line-before": [],
+    "block-no-empty": null
+  }
+}
