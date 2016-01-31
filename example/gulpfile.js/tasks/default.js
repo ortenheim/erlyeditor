4b448cd2037df7078bafa347a6926ede008d1d78
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2016-01-31 19:41:35 +0300

    chore(test): Use ignore-styles instead of custom compiler for mocha

--- example/gulpfile.js/tasks/default.js
+++ example/gulpfile.js/tasks/default.js
@@ -2,5 +2,7 @@ gulp.task('default', () => sequence(
   'assets',
   'bundle:server',      // build server bundle
   'server',             // start server
-  'sync:proxy'          // start browsersync as proxy to express server
+
+  // start browsersync as proxy to express server
+  'sync:proxy'
 ));
